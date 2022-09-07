6022015b0c07031213003b77a598b273c832415f
21winster <113053566+21winster@users.noreply.github.com>
2022-09-07 23:36:34 +0530

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,12 @@
+print("Weather Report")
+temp = 5
+if temp >50:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temp > 20:
+    print("its a nice day")
+elif temp > 10:
+    print("its a cold day")
+else:
+    print("its cold")
+print("done")
